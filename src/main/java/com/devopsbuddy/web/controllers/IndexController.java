a308fc9f1b4ae7d3b6b490dddc4041117d9a7241
JeanA <jean.arbonel@gmail.com>
2018-04-30 11:42:56 -0400

    added controller

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,6 +1,6 @@
 package com.devopsbuddy.web.controllers;
 
-import org.springframework.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
